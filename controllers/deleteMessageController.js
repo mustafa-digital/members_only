5679a3a1d867892f47ab6f9e7b5a855e38413f1f
Mustafa <124535608+MUChappy@users.noreply.github.com>
2024-09-03 14:10:21 -0400

    Add start script in package.json Added a start script for deploying to railway

--- controllers/deleteMessageController.js
+++ controllers/deleteMessageController.js
@@ -1,12 +1,12 @@
 /**
  * -------------- deleteMessageController.js ----------------
+ * This controller just deals with posts to /delete-message routes, for admins to delete messages
  */
 const { deleteMessage } = require("../lib/queries");
 
 const deleteMessageController = {
   post: async (req, res, next) => {
     const messageId = req.params.messageId;
-    // remove the message from the db
     try {
       await deleteMessage(messageId);
       res.redirect("/view-messages");
